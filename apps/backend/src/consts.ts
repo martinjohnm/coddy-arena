258642608a3a53c28e441c53e5b2ed5fd299f2b0
Martin John M <johnmmartin74@gmail.com>
2025-01-06 12:36:34 +0100

    backend project initialized and build script added to package.json

---
+++ apps/backend/src/consts.ts
@@ -0,0 +1,3 @@
+
+
+export const COOKIE_MAX_AGE = 24 * 60 * 60 * 1000;
---
+++ apps/backend/src/index.ts
@@ -0,0 +1,39 @@
+
+
+import { configDotenv } from "dotenv";
+import express from "express"
+import cookieParser from "cookie-parser"
+import session from "express-session";
+import cors from "cors"
+import { COOKIE_MAX_AGE } from "./consts";
+
+const app = express();
+configDotenv()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(
+    session({
+        secret : process.env.COOKIE_SECRET || "cookie-secret",
+        resave : false,
+        saveUninitialized : false,
+        cookie : { secure : false, maxAge : COOKIE_MAX_AGE}
+    })
+)
+const allowedHosts = process.env.ALLOWED_HOSTS
+  ? process.env.ALLOWED_HOSTS.split(',')
+  : [];
+
+app.use(
+    cors({
+        origin : allowedHosts,
+        methods: 'GET,POST,PUT,DELETE',
+        credentials: true
+    })
+);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Backend server is running on port ${PORT}`);
+});
